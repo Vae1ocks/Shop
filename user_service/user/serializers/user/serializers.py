c58e6d28e041e3e917eba7d011eb915b44ba74fc
Ophely-git <ophely1992@gmail.com>
2024-07-31 18:49:08 +0300

    add user_service

--- user_service/user/serializers/user/serializers.py
+++ user_service/user/serializers/user/serializers.py
@@ -13,3 +13,49 @@ class UserRepresentationalInfo(ModelSerializer):
         model = get_user_model()
         fields = ['first_name', 'profile_picture']
 
+
+class GetLoginTokenSerializer(Serializer):
+    email = EmailField(write_only=True, required=True)
+    password = CharField(write_only=True, required=True)
+
+
+class GetUserInfoSerializer(ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ['pk', 'email', 'first_name', 'profile_picture', 'categories_bought', 'coupon_balance']
+
+
+class UserEditNamePictureSerializer(ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ['first_name', 'profile_picture']
+
+
+class EditUserSendEmailSerializer(Serializer):
+    user_email = EmailField(write_only=True, required=True)
+
+
+class ConfirmEditUserEmailSerializer(Serializer):
+    short_code = CharField(write_only=True, required=True)
+
+
+class EditUserEmailSerializer(ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ['email']
+
+
+class EditUserPasswordSerializer(Serializer):
+    old_password = CharField(write_only=True, required=True)
+    new_password = CharField(write_only=True, required=True)
+    repeat_password = CharField(write_only=True, required=True)
+
+    def validate(self, attrs):
+        if attrs['old_password'] == attrs['new_password']:
+            raise ValidationError('Неверно введен новый пароль.')
+        if attrs['new_password'] != attrs['repeat_password']:
+            raise ValidationError('Новые пароли не совпадают.')
+        if attrs['new_password'].isdigit() or len(attrs['new_password']) < 8:
+            raise ValidationError('Новый пароль состоит только из цифр или меньше 8 символов.')
+        return attrs
+
--- user_service/user/views/user/urls.py
+++ user_service/user/views/user/urls.py
@@ -6,5 +6,13 @@ urlpatterns = [
          name='categories_bouth_by_user'),
     path('<int:pk>/representational-data/', UserRepresentationalView.as_view(),
          name='user_representational-data'),
+    path('detail/', GetUserInfoView.as_view()),
+    path('edit-user/fn-pct/', EditUserNamePictureView.as_view()),
+    path('edit-user/pass/', EditUserPasswordView.as_view()),
+    # change email
+    path('change-email/send-mail/', EditUserSendEmailView.as_view()),
+    path('change-email/send-mail/confirm/', ConfirmEditUserEmailView.as_view()),
+    path('change-email/send-mail/new-send/', SendMailNewEmailView.as_view()),
+    path('change-email/send-mail/set-email/', SetNewEmailView.as_view()),
 
 ]
