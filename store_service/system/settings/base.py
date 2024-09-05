b61e415b2e469b4151fdc5faee22e594bff43a07
Vae1ocks <antondobranickij@gmail.com>
2024-09-05 15:06:33 +0300

    поправил импорты

--- store_service/system/settings/base.py
+++ store_service/system/settings/base.py
@@ -2,18 +2,13 @@ from pathlib import Path
 from datetime import timedelta
 import os
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-6twwvifhv(wf*hmlyxd8368k@4ps9_6o$$*jm9#k!_-%mn(bff'
 JWT_SECRET_KEY = 'django-insecure-stj&7uj5z2pf=rrcj-5(%lyy6^eh6%2r0$ni_z3ro@c8i%vms1'
 
-# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -151,4 +146,3 @@ CELERY_RESULT_BACKEND = "redis://localhost:6379"
 # CORS
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
-
