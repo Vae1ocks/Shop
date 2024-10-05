622bcfad22ce0fdfd1c9eddcde805af1ca6f2a78
Oleg Shchegolev <themadhouse@mail.ru>
2024-10-05 20:50:29 +0000

    feat(profile): получение активного роута для хлебных крошек

--- frontend/src/app/pages/profile/src/lib/profile/profile.component.ts
+++ frontend/src/app/pages/profile/src/lib/profile/profile.component.ts
@@ -1,23 +1,43 @@
-import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import {
+  NavigationEnd,
+  Router,
+  RouterLink,
+  RouterLinkActive,
+  RouterOutlet,
+} from '@angular/router';
+import { ROUTE_TOKENS } from '@app/shared/app-config';
+import { filter, map } from 'rxjs/operators';
 
 @Component({
+  changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'lib-profile',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, RouterLink],
+  imports: [RouterOutlet, CommonModule, RouterLink, RouterLinkActive],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss',
 })
+export class ProfileComponent {
+  private readonly ROUTE_TOKENS = ROUTE_TOKENS;
 
-export class ProfileComponent implements OnInit {
- public link = ''
-  constructor() {
-  }
+  private readonly activeRouteName: Record<string, string> = {
+    [this.ROUTE_TOKENS.PROFILE.USER]: 'Профиль',
+    [this.ROUTE_TOKENS.PROFILE.HISTORY]: 'История',
+  };
 
-  ngOnInit() {
+  private readonly router = inject(Router);
 
-  }
+  public link = '';
 
-
+  readonly activeUrl$$ = toSignal(
+    this.router.events.pipe(
+      filter((event): event is NavigationEnd => event instanceof NavigationEnd),
+      map((event) => {
+        const urlSegment = event.url.split('/').at(-1);
+        return urlSegment ? this.activeRouteName[urlSegment] : '';
+      }),
+    ),
+  );
 }
